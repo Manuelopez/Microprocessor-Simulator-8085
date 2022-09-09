bb5bebcb610bab093000e0bf54e92780ea884841
Manuel Arturo Lopez Perez <manuelopez@Manuels-MacBook-Air.local>
2022-09-09 11:43:50 -0400

    adding missing orl codes

--- opcode/and.go
+++ opcode/and.go
@@ -1,25 +1,25 @@
 package opcode
 
 const (
-  AND_AL_AH = 0x24
-  AND_AL_B = 0x25
-  AND_AL_C = 0x26
-  AND_AL_D = 0x27
-  AND_AL_E = 0x28
-  AND_AL_L = 0x29
-  AND_AL_H = 0x2A
-  AND_AL_M = 0x2C
-  AND_AL_VAL = 0x2E
-  )
+	AND_AL_AH  = 0x24
+	AND_AL_B   = 0x25
+	AND_AL_C   = 0x26
+	AND_AL_D   = 0x27
+	AND_AL_E   = 0x28
+	AND_AL_L   = 0x29
+	AND_AL_H   = 0x2A
+	AND_AL_M   = 0x2C
+	AND_AL_VAL = 0x2E
+)
 
 var AND = map[string]byte{
-  "AL": AND_AL_AH,
-  "B": AND_AL_B,
-  "C": AND_AL_C,
-  "D": AND_AL_D,
-  "E": AND_AL_E,
-  "L": AND_AL_L,
-  "H": AND_AL_H,
-  "M": AND_AL_M,
-  "VAL": AND_AL_VAL,
+	"AL":  AND_AL_AH,
+	"B":   AND_AL_B,
+	"C":   AND_AL_C,
+	"D":   AND_AL_D,
+	"E":   AND_AL_E,
+	"L":   AND_AL_L,
+	"H":   AND_AL_H,
+	"M":   AND_AL_M,
+	"VAL": AND_AL_VAL,
 }
--- opcode/orl.go
+++ opcode/orl.go
@@ -1,23 +1,25 @@
 package opcode
 
 const (
-	ORL_AL_AH = 0x1D
-	ORL_AL_B  = 0x1E
-	ORL_AL_C  = 0x1F
-	ORL_AL_D  = 0x20
-	ORL_AL_E  = 0x21
-	ORL_AL_L  = 0x22
-	ORL_AL_H  = 0x23
-  
+	ORL_AL_AH  = 0x1D
+	ORL_AL_B   = 0x1E
+	ORL_AL_C   = 0x1F
+	ORL_AL_D   = 0x20
+	ORL_AL_E   = 0x21
+	ORL_AL_L   = 0x22
+	ORL_AL_H   = 0x23
+	ORL_AL_M   = 0x2B
+	ORL_AL_VAL = 0x2D
 )
 
 var ORL = map[string]byte{
-	"AH": ORL_AL_AH,
-	"B":  ORL_AL_B,
-	"C":  ORL_AL_C,
-	"D":  ORL_AL_D,
-	"E":  ORL_AL_E,
-	"L":  ORL_AL_L,
-	"H":  ORL_AL_H,
-
+	"AH":  ORL_AL_AH,
+	"B":   ORL_AL_B,
+	"C":   ORL_AL_C,
+	"D":   ORL_AL_D,
+	"E":   ORL_AL_E,
+	"L":   ORL_AL_L,
+	"H":   ORL_AL_H,
+	"M":   ORL_AL_M,
+	"VAL": ORL_AL_VAL,
 }
